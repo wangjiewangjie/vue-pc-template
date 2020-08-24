97ed4ec3ae7376264a9223b34342271299b4face
wangjie <wangjie0107@aliyun.com>
2020-08-24 09:51:54 +0800

    config🔧 移除default-passive-events库

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import "./plugins/element.js";
 import "./plugins/charts.js";
 import "./styles/reset.css";
 import "../mock";
-import "default-passive-events"; //解决Chrome 增加了新的事件捕获机制－Passive Event Listeners警告
 import * as api from "@/api/api";
 import "../public/icon/iconfont.css";
 import "../public/icon/iconfont.js";
